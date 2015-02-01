0dfe4d2d9a7981205213ed29656c7cdbfb40bcc5
David Cai <dcai@financialengines.com>
2015-01-31 21:41:42 -0800

    added bower.js and removed gulp-flatten

---
+++ gulp/bower.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var gulp = require('gulp');
+var shell = require('gulp-shell');
+
+/**
+ * Remove then install required bower components
+ */
+gulp.task('bower:refresh', shell.task([
+  'echo Deleting bower components ...',
+  'rm -rf bower_components',
+  'echo Installing bower components ...',
+  'bower install'
+]));
+
+
+/**
+ * Register component in bower registry
+ */
+// gulp.task('bower:register', function() {
+  // TODO
+// });
