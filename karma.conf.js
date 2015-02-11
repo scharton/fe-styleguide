acc15075157dfa1f77d2972e0dcd897d47f20f1a
Luke <lmertens@financialengines.com>
2015-02-11 12:56:14 -0800

    fixed white space and added jenkins xml compatible coverage report

--- karma.conf.js
+++ karma.conf.js
@@ -6,23 +6,31 @@ module.exports = function(config) {
     plugins : [
       'karma-phantomjs-launcher',
       'karma-jasmine',
-	  'karma-coverage',
-	  'karma-junit-reporter'
+      'karma-coverage',
+      'karma-junit-reporter'
     ],
     autoWatch : false,
     frameworks: ['jasmine'],
     browsers : ['PhantomJS'],
-	reporters: ['progress', 'junit', 'coverage'],
-	preprocessors:    {
-		'src/app/**/{*.js,!(*.spec.js)}':   ['coverage']
-	},
-	junitReporter: {
-		outputFile: 'reports/junit/TESTS-xunit.xml'
-	},
-	coverageReporter: {
-		type:   'lcov',
-		dir:    'reports',
-		subdir: 'coverage'
-	}
+    reporters: ['progress', 'junit', 'coverage'],
+    preprocessors:    {
+      'src/app/**/{*.js,!(*.spec.js)}':   ['coverage']
+    },
+    junitReporter: {
+      outputFile: 'reports/unit/xunit.xml'
+    },
+    coverageReporter: {
+      dir: 'reports/coverage',
+      reporters: [
+        {
+          type: 'html',
+          subdir: '.'
+        },
+        {
+          type: 'cobertura',
+          subdir: '.'
+        }
+      ]
+    }
   });
 };
