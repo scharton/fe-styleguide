756115c8e6072141184a13d1cb495eea4a465b52
David Cai <dcai@financialengines.com>
2015-02-12 15:41:23 -0800

    added fe-env

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 var gulp = require('gulp');
 
 require('require-dir')('./gulp');
+require('./bower_components/fe-env/dist/gulp/setenv');
 
 gulp.task('default', ['clean'], function () {
   gulp.start('build');
