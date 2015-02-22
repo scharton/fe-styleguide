04bdf924219b84cf9ed057b71bf9bc3b90eb23fc
Min Zhu <mzhu@FinancialEngines.com>
2015-02-21 17:20:14 -0800

    add angular progressbar

--- src/app/components/details/progressbar.controller.js
+++ src/app/components/details/progressbar.controller.js
@@ -1,4 +1,27 @@
 'use strict';
 
 angular.module('styleguide')
-  .controller('ProgressBarCtrl', ProgressBarCtrl)
+  .controller('progress1Ctrl', function($scope) {
+
+  	$scope.value = 10;
+  	$scope.items = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+
+  	$scope.type = 'primary';
+
+  	$scope.$watch('value',function(val){
+       $scope.value = parseInt(val); 
+    });
+  
+  })
+  .controller('progress2Ctrl', function($scope) {
+
+  	$scope.value = 30;
+  	$scope.items = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+  	$scope.type = 'success';
+  	$scope.dynamicTooltip= 'You have achieved ' + $scope.value + '% .';
+
+  	$scope.$watch('value', function(val){
+       $scope.value = parseInt(val); 
+    });
+ 
+  });
