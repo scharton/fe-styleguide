ebd9b7b2efca4f8ace6f381edb04664023940902
wcheng <wcheng@financialengines.com>
2015-03-02 12:03:54 -0800

    get e2e test to pass

--- e2e/home.spec.js
+++ e2e/home.spec.js
@@ -11,7 +11,7 @@ describe('The home view', function () {
   });
 
   it('should include jumbotron with correct data', function() {
-    expect(page.title.getText()).toBe('FE New Tech Stack');
+    expect(page.title.getText()).toBe('The Financial Engines New Tech Stack');
   });
 
   it('list more than 5 awesome things', function () {
