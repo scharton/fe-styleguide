94c8407c6e71da0f8ee72755efd20f714021bb0a
David Cai <dcai@financialengines.com>
2015-05-01 10:53:43 -0700

    integrated fe-angular-core and fe-gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,20 @@
 'use strict';
 
+var options;
+
+require('fe-gulp')(function(opts) {
+  opts.angular = {
+    module: 'styleguide'
+  };
+
+  options = opts;
+
+  return opts;
+});
+
+
 var gulp = require('gulp');
 
-require('require-dir')('./gulp');
-require('./bower_components/fe-env/dist/scripts/server/gulp');
-
-gulp.task('default', ['clean'], function () {
+gulp.task('default', ['clean'], function() {
   gulp.start('build');
 });
