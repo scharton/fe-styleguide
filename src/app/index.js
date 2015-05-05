47091669f2f9ecea81a8196ff504525a43348136
David Cai <dcai@financialengines.com>
2015-05-05 15:25:16 -0700

    added DTM

--- src/app/index.js
+++ src/app/index.js
@@ -1,12 +1,34 @@
-'use strict';
+(function() {
+  'use strict';
 
-angular.module('styleguide', [
-  'ngSanitize',
-  'ngResource',
-  'ui.router',
-  'ui.bootstrap',
-  'feSliders'])
+  angular
+    .module('styleguide', [
+      'ngSanitize',
+      'ngResource',
+      'ui.router',
+      'ui.bootstrap',
+      'feSliders'])
+    .config(config)
+    .controller('MainCtrl', MainCtrl);
 
-  .config(function($urlRouterProvider) {
+
+  function config($urlRouterProvider) {
     $urlRouterProvider.otherwise('/');
-  });
+  }
+
+
+  function MainCtrl($rootScope, $log) {
+
+    if (_satellite) {
+
+      $rootScope.$on('$stateChangeSuccess',
+        function(event, toState/*, toParams, fromState, fromParams*/) {
+          $log.log('toState = ', toState.name);
+
+          _satellite.setVar('stateName', toState.name);
+          _satellite.track('spaStateChange');
+        });
+    }
+  }
+
+})();
