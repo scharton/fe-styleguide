11ca42a7e452ec463a365dc164c13346d0d8bc61
David Cai <dcai@financialengines.com>
2015-05-13 18:53:15 -0700

    added view-flipper

--- src/app/experiments/view-flipper/view-flipper.controller.js
+++ src/app/experiments/view-flipper/view-flipper.controller.js
@@ -6,8 +6,49 @@
     .controller('ViewFlipperCtrl', ViewFlipperCtrl);
 
 
-  function ViewFlipperCtrl() {
+  function ViewFlipperCtrl($scope) {
 
+    //
+    // Flip
+    //
+
+    var elFlipC = angular.element('.view-flipper');
+
+    $scope.flip = function() {
+      elFlipC.toggleClass('flipped');
+    };
+
+
+    //
+    // Chart
+    //
+
+    $scope.svgSupport = Modernizr.svg;
+
+    if ($scope.svgSupport) {
+
+      $scope.chart = {
+        data: {
+          columns: [
+              ['data1', 30, 20, 50, 40, 60, 50],
+              ['data2', 200, 130, 90, 240, 130, 220],
+              ['data3', 300, 200, 160, 400, 250, 250],
+              ['data4', 200, 130, 90, 240, 130, 220],
+              ['data5', 130, 120, 150, 140, 160, 150],
+              ['data6', 90, 70, 20, 50, 60, 120],
+          ],
+          type: 'bar',
+          types: {
+              data3: 'spline',
+              data4: 'line',
+              data6: 'area',
+          },
+          groups: [
+              ['data1','data2']
+          ]
+        }
+      }; // /$scope.chart
+    }
   }
 
 })();
